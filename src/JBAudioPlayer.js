0920162b5ff161b420bd006ac06cf35b418d6b85
Joe Billman <mrjoebillman@gmail.com>
2016-04-11 17:32:49 -0600

    Added the remaining createjs TypeScript files and made some minor changes.

--- src/JBAudioPlayer.js
+++ src/JBAudioPlayer.js
@@ -1,3 +1,4 @@
+/// <reference path="../libraries/createjs/createjs.d.ts"/>
 /**
  * Created by Joe on 4/11/2016.
  */
--- src/JBAudioPlayer.ts
+++ src/JBAudioPlayer.ts
@@ -1,3 +1,5 @@
+/// <reference path="../libraries/createjs/createjs.d.ts"/>
+
 /**
  * Created by Joe on 4/11/2016.
  */
