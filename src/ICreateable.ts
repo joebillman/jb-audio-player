8d6d51241c8967d9d4a7a199d161fceab76e4f41
Joe Billman <mrjoebillman@gmail.com>
2016-04-11 18:00:34 -0600

    Added some more initial functionality.

---
+++ src/ICreateable.ts
@@ -0,0 +1,8 @@
+/**
+ * Created by Joe on 4/11/2016.
+ */
+
+interface ICreateable
+{
+    create();
+}
--- src/JBAudioPlayer.js
+++ src/JBAudioPlayer.js
@@ -1,5 +1,34 @@
 /// <reference path="../libraries/createjs/createjs.d.ts"/>
+/// <reference path="ICreateable.ts"/>
 /**
  * Created by Joe on 4/11/2016.
  */
+var JBAudioPlayer = (function () {
+    //--------------------------------------------------------------------------
+    //
+    //  Constructor
+    //
+    //--------------------------------------------------------------------------
+    function JBAudioPlayer() {
+    }
+    //--------------------------------------------------------------------------
+    //
+    //  Methods
+    //
+    //--------------------------------------------------------------------------
+    //----------------------------------
+    //  Private:
+    //----------------------------------
+    JBAudioPlayer.prototype._init = function () {
+    };
+    //----------------------------------
+    //  Public:
+    //----------------------------------
+    JBAudioPlayer.prototype.create = function () {
+        alert("create");
+    };
+    JBAudioPlayer.prototype.play = function () {
+    };
+    return JBAudioPlayer;
+}());
 //# sourceMappingURL=JBAudioPlayer.js.map
